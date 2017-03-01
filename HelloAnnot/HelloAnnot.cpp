b3dd1f58b3dbeb29d289c89857cc636f04a9693b
Basha133 <test@nikola03.xeno.cl.cam.ac.uk>
2017-03-01 18:07:22 +0000

    HelloAnnot and TARuntimeCalls added

---
+++ HelloAnnot/HelloAnnot.cpp
@@ -0,0 +1,134 @@
+#include <fstream>
+#include <map>
+#include <string>
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DebugInfoMetadata.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IntrinsicInst.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+using namespace std;
+
+namespace {
+struct Hello : public ModulePass {
+  static char ID;
+  Hello() : ModulePass(ID) {}
+
+  bool runOnModule(Module &M) override {
+    ifstream loom_inst_basic_policy;
+    ofstream loom_inst_policy;
+    loom_inst_basic_policy.open ("basic.policy");
+    loom_inst_policy.open ("loom_inst.policy");
+    char c;
+    //Copy base of our loom policy into a new loom policy
+    while (!loom_inst_basic_policy.eof()) {
+      loom_inst_basic_policy.get(c);
+      loom_inst_policy << c;
+    }
+    string ta_prefix = "TA_ASSERT";
+    string ta_instrument_anno = "TA_INSTRUMENT";
+    bool functions_instrumented = false;
+    
+    //Look through global constants to look for function annotations
+    auto global_annos = M.getNamedGlobal("llvm.global.annotations");
+    if (global_annos) {
+      auto a = cast<ConstantArray>(global_annos->getOperand(0));
+      for (unsigned int i=0; i<a->getNumOperands(); i++) {
+	      auto e = cast<ConstantStruct>(a->getOperand(i));
+      	if (auto fn = dyn_cast<Function>(e->getOperand(0)->getOperand(0))) {
+          auto anno = cast<ConstantDataArray>(cast<GlobalVariable>(
+      	  e->getOperand(1)->getOperand(0))->getOperand(0))->getAsCString();
+
+      	  //Check whether it starts with our prefix
+      	  string std_anno = anno.str();
+      	  if (std_anno.compare(0, ta_prefix.length(), ta_prefix) == 0) {
+                  functions_instrumented = true;    
+                  fn->addFnAttr(ta_instrument_anno, anno);
+      	  }
+      	}
+      }
+    }
+    if (functions_instrumented) {
+        loom_inst_policy << "functions:\n";
+    }
+    
+    //Manage function annotations - generate policies
+    for (auto cur_fref = M.getFunctionList().begin(),
+         end_fref = M.getFunctionList().end();
+         cur_fref != end_fref; ++cur_fref) {
+      //errs() << "HelloAnnot: funct " << cur_fref->getName() << "\n";
+      if (cur_fref->hasFnAttribute(ta_instrument_anno)) {
+        errs() << "HelloAnnot: " << cur_fref->getName() << "\n";
+        loom_inst_policy << "    - name: " << cur_fref->getName().str() << "\n";
+        loom_inst_policy << "      caller: [ entry ]" << "\n";
+      }
+    }
+    
+    map<Value*, DbgDeclareInst*> value_to_dbg_dec;
+    bool vars_instrumented = false;
+
+    for (auto cur_f = M.getFunctionList().begin(),
+         end_f = M.getFunctionList().end();
+	 cur_f != end_f; ++cur_f) {
+      errs() << "HelloAnnot: going over " << cur_f->getName() << "\n";
+      for (auto cur_b = cur_f->getBasicBlockList().begin(),
+           end_b = cur_f->getBasicBlockList().end();
+           cur_b != end_b; ++cur_b) {
+        for (auto cur_i = cur_b->getInstList().begin(),
+             end_i = cur_b->getInstList().end();
+             cur_i != end_i; ++cur_i) {
+	  if (auto cur_dbg_dec = dyn_cast<DbgDeclareInst>(cur_i)) {
+	    errs() << "Found declaration: "
+		<< cur_dbg_dec->getVariable()->getName().str()
+		<< " on address " << (void*) cur_dbg_dec->getAddress() << "\n";
+	    value_to_dbg_dec[cur_dbg_dec->getAddress()] = cur_dbg_dec;
+	  }
+          if (auto cur_call = dyn_cast<CallInst>(cur_i)) {
+            errs() << "Found a call: ";
+            errs() << cur_call->getCalledFunction()->getName() << "\n";
+	    if (cur_call->getCalledFunction()->getName() == "llvm.var.annotation") {
+	      errs() << "Found annotation on value " << (void*) cur_call->getArgOperand(0)->stripPointerCasts() << "\n";
+	      if (value_to_dbg_dec.find(cur_call->getArgOperand(0)->stripPointerCasts()) != value_to_dbg_dec.end()) {
+                errs() << "Should annotate " << value_to_dbg_dec[cur_call->getArgOperand(0)->stripPointerCasts()]->getVariable()->getName().str() << "\n";
+		if (auto gv = dyn_cast<ConstantExpr>(cur_call->getArgOperand(1))) {
+		  string std_anno = cast<ConstantDataArray>(cast<GlobalVariable>(cast<ConstantExpr>(gv)->getOperand(0))->getInitializer())->getAsCString();
+		  /*if (std_anno.compare(0, ta_prefix.length(), ta_prefix) == 0) {
+            	    if (!vars_instrumented) {
+		      loom_inst_policy << "structures:\n"
+		      vars_instrumented = true;
+		    }
+            	    fn->addFnAttr(ta_instrument_anno);
+            	    fn->addFnAttr(anno);
+	  	  }*/
+		  errs() << "with " << std_anno << "\n";
+		}
+	      }
+	      /*for (unsigned int i=0; i<cur_call->getNumArgOperands(); i++) {
+		Value *op = cur_call->getArgOperand(i);
+		errs() << " " << (op->hasName()? op->getName().str() : "noname"); 
+	      }*/
+	      errs() << "\n";
+	    }
+          }
+          //errs() << cur_i->getOpcodeName() << "\n";
+        }
+      }
+    }
+
+    //errs() << "HelloAnnot: ";
+    //errs().write_escaped(F.getName()) << '\n';
+    loom_inst_basic_policy.close();
+    loom_inst_policy.close();
+    return false;
+  }
+}; // end of struct Hello
+}  // end of anonymous namespace
+
+char Hello::ID = 0;
+static RegisterPass<Hello> X("hello_annot", "Hello World Pass",
+                             false /* Only looks at CFG */,
+                             false /* Analysis Pass */);
---
+++ TARuntimeCalls/TARuntimeCalls.cpp
@@ -0,0 +1,124 @@
+#include <fstream>
+#include <map>
+#include <string>
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DebugInfoMetadata.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IntrinsicInst.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+using namespace std;
+
+namespace {
+
+pair<string, int> getNextStringPart(const string& formatted_string, char delim, int from_pos) {
+  string res;
+  int i;
+  for (i=from_pos; i<formatted_string.length(); i++) {
+    if (formatted_string[i] == delim) {
+      break;
+    }
+    res.push_back(formatted_string[i]);
+  }
+  return make_pair(res, i);
+}
+
+int getArgNumFromFormat(string formatted_string) {
+  pair<string, int> ta_const = getNextStringPart(formatted_string,':',0);
+  pair<string, int> assert_type = getNextStringPart(formatted_string,':',ta_const.second+1);
+
+  if (!assert_type.first.compare("arg_monotonic")) {
+    pair<string, int> arg_num = getNextStringPart(formatted_string,':',assert_type.second+1);
+    return stoi(arg_num.first);
+  } else {
+    printf("Unrecognised assertion type: %s\n", assert_type.first.c_str());
+  }
+  return -1;
+}
+
+struct TARuntimeCalls : public ModulePass {
+  static char ID;
+  TARuntimeCalls() : ModulePass(ID) {}
+
+  bool runOnModule(Module &M) override {
+    string ta_prefix = "TA_ASSERT";
+    string ta_instrument_anno = "TA_INSTRUMENT";
+    StringRef ta_inst_anno_ref = StringRef(ta_instrument_anno);
+    
+    //Manage function annotations - generate calls to assertion manager
+    for (auto cur_f = M.getFunctionList().begin(),
+         end_f = M.getFunctionList().end();
+	       cur_f != end_f; ++cur_f) {
+      //errs() << "RuntimeCalls: funct " << cur_fref->getName() << "\n";
+      if (cur_f->hasFnAttribute(ta_instrument_anno)) {
+        errs() << "RuntimeCalls: " << cur_f->getName() << "\n";
+        string loom_fun_name = "__loom_call_" + cur_f->getName().str();
+        Function *loom_f = M.getFunction(loom_fun_name);
+        
+        //Remove return from the generated function - we should add our instructions
+        //in its place
+        BasicBlock &entry_block = loom_f->getEntryBlock();
+        Instruction &last_inst = entry_block.back();
+        last_inst.eraseFromParent();
+        
+        IRBuilder<> builder(&entry_block);
+        
+        //Function is external, so lets declare it
+        LLVMContext& context = M.getContext();
+        std::vector<llvm::Type *> newValue_args;
+        newValue_args.push_back(Type::getInt8PtrTy(context));
+        newValue_args.push_back(Type::getInt32Ty(context));
+        FunctionType *newValue_type =
+              FunctionType::get(Type::getVoidTy(context), newValue_args, false);
+        Constant *newValue_fun =
+              M.getOrInsertFunction("AssertManager_newValue", newValue_type);
+
+        //Value *hello_str = builder.CreateGlobalStringPtr("Instrumentation in action! \n");
+        //Value *hello_str = builder.CreateGlobalStringPtr(cur_f->getName().str());
+        
+
+        Attribute form_att = cur_f->getFnAttribute(ta_inst_anno_ref);
+        Value *hello_str = builder.CreateGlobalStringPtr(cur_f->getName().str() + ":" + form_att.getValueAsString().str());
+
+        std::vector<llvm::Value *> fun_args;
+        fun_args.push_back(hello_str);
+
+        //Figure out which (if any) argument we want to pass along
+        int arg_num = getArgNumFromFormat(form_att.getValueAsString().str());
+        
+        Value* x = ConstantInt::getSigned(Type::getInt32Ty(context), 0);
+        if (arg_num >= 0) {
+          Function::arg_iterator call_args = loom_f->arg_begin();
+          for (int i=0; i<arg_num; i++) {
+            call_args++;
+          }
+          x = &(*call_args);
+        }
+
+        fun_args.push_back(x);
+        //Constant *printfFunc = M.getFunction("printf");
+        //builder.CreateCall(printfFunc, fun_args);
+        //Constant *assert_manager_call = M.getFunction("AssertManager_newValue");
+        
+        builder.CreateCall(newValue_fun, fun_args);
+        builder.CreateRetVoid();
+        //loom_inst_policy << "    - name: " << cur_fref->getName().str() << "\n";
+        //loom_inst_policy << "      caller: [ entry ]" << "\n";
+      }
+    }
+    
+    return false;
+  }
+}; // end of struct TARuntimeCalls
+}  // end of anonymous namespace
+
+char TARuntimeCalls::ID = 0;
+static RegisterPass<TARuntimeCalls> X("runtime_calls", "Temporal Assertions 2nd pass",
+                             false /* Only looks at CFG */,
+                             false /* Analysis Pass */);
