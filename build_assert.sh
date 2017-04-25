d0e94ae1ba0b891c54e3896afa3f24b0f6d6ff49
Basha133 <basha.kovacova@gmail.com>
2017-04-25 19:20:10 +0000

    another build script addded

---
+++ build_assert.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+cd ~/project/sandbox
+
+clang -g -O0 -emit-llvm ${1}.c -c -o ${1}.bc
+
+opt -load ~/llvm/Release/lib/ProjectHelloAnnot.so -hello_annot < ${1}.bc > ${1}-annot.bc
+
+opt -load /home/test/loom/Release/lib/LLVMLoom.so -loom -loom-file loom_inst.policy < ${1}-annot.bc > ${1}-loomed.bc
+
+opt -load ~/llvm/Release/lib/TARuntimeCalls.so -runtime_calls < ${1}-loomed.bc > ${1}.bc
+
+llc -filetype=obj ${1}-final.bc
+
+cp funct-assert2-final.o ~/project/funct-assert2-final.o
+
+cd ~/project
+
+clang -L. -Wl,-rpath=. -lassert_manager_clib -lpmc -o ${1} ${1}-final.o
+
