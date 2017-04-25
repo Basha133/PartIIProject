c3f8f5323ca705f2bd1c2978083c72150217660c
Basha133 <basha.kovacova@gmail.com>
2017-04-25 16:11:12 +0000

    script to build runtime library added

---
+++ build_runtime_lib.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+clang++ -fpic -c monotonic_variable.cpp frequency_variable.cpp variable.cpp assert_manager.cpp common_util.cpp output_logger.cpp
+
+clang++ -shared -o libassert_manager.so variable.o monotonic_variable.o frequency_variable.o assert_manager.o common_util.o output_logger.o
+
+clang -fpic -shared assert_manager_clib.cpp -L. -Wl,-rpath=. -lassert_manager -o libassert_manager_clib.so
+
